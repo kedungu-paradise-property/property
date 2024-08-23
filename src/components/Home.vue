137340d5304530ec0f934fa64435a182f83f160c
nobelsaputra <kedunguparadise@gmail.com>
2024-08-23 17:26:25 +0800

    push

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center justify-center min-h-screen bg-gray-100">
     <div class="container mx-auto px-4">
-      <div class="flex flex-wrap -mx-4">
+      <div class="flex flex-wrap -mx-4 max-sm:mt-16">
         <!-- Card 1 -->
         <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-4">
           <div
@@ -12,20 +12,28 @@
               <div class="p-8 rounded-t-lg bg-gray-300 h-48"></div>
             </a>
             <div class="px-5 pb-5">
-              <p>
-                <h5
-                  class="text-xl mb-6 mt-2 font-semibold tracking-tight text-gray-900 dark:text-white"
-                >
-                  Riverside Paradise in Kedungu
-                </h5>
-            </p>
+              <h5
+                class="text-xl mt-2 font-semibold tracking-tight text-gray-900 dark:text-white"
+              >
+                Riverside Paradise in Kedungu
+              </h5>
+              <h5
+                class="text-md mt-2 font-semibold tracking-tight text-gray-600 dark:text-white"
+              >
+                size : 1,460m² + (140m² private road access)
+              </h5>
+              <h5
+                class="text-md mb-6 mt-2 font-semibold tracking-tight text-gray-600 dark:text-white"
+              >
+               dimension : 41 x 40 meters
+              </h5>
               <div class="flex items-center justify-between">
-                <span class="text-xl font-medium text-gray-900 dark:text-white"
-                  >Rp13,900,000</span
-                >
+                <span class="text-lg font-medium text-gray-900 dark:text-white">
+                  Rp13,900,000/are/year
+                </span>
                 <a
                   href="#"
-                  class="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   Detail
                 </a>
@@ -51,102 +59,9 @@
                 </h5>
               </a>
               <div class="flex items-center justify-between">
-                <span class="text-3xl font-bold text-gray-900 dark:text-white"
-                  >$599</span
-                >
-                <a
-                  href="#"
-                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Add to cart
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- card 3 -->
-        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-4">
-          <div
-            class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-          >
-            <a href="#">
-              <!-- Kosongkan gambar -->
-              <div class="p-8 rounded-t-lg bg-gray-300 h-48"></div>
-            </a>
-            <div class="px-5 pb-5">
-              <a href="#">
-                <h5
-                  class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
-                >
-                  Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-                </h5>
-              </a>
-              <div class="flex items-center justify-between">
-                <span class="text-3xl font-bold text-gray-900 dark:text-white"
-                  >$599</span
-                >
-                <a
-                  href="#"
-                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Add to cart
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- card 4 -->
-        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-4">
-          <div
-            class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-          >
-            <a href="#">
-              <!-- Kosongkan gambar -->
-              <div class="p-8 rounded-t-lg bg-gray-300 h-48"></div>
-            </a>
-            <div class="px-5 pb-5">
-              <a href="#">
-                <h5
-                  class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
-                >
-                  Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-                </h5>
-              </a>
-              <div class="flex items-center justify-between">
-                <span class="text-3xl font-bold text-gray-900 dark:text-white"
-                  >$599</span
-                >
-                <a
-                  href="#"
-                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Add to cart
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- card 4 -->
-        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-4">
-          <div
-            class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-          >
-            <a href="#">
-              <!-- Kosongkan gambar -->
-              <div class="p-8 rounded-t-lg bg-gray-300 h-48"></div>
-            </a>
-            <div class="px-5 pb-5">
-              <a href="#">
-                <h5
-                  class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
-                >
-                  Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-                </h5>
-              </a>
-              <div class="flex items-center justify-between">
-                <span class="text-3xl font-bold text-gray-900 dark:text-white"
-                  >$599</span
-                >
+                <span class="text-3xl font-bold text-gray-900 dark:text-white">
+                  $599
+                </span>
                 <a
                   href="#"
                   class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
