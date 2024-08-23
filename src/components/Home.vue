6c3b9d5941c80227b92604abcbc1fa0ccee33e8f
nobelsaputra <nobelsaputra10@gmail.com>
2024-08-23 16:13:43 +0800

    kedungu

---
+++ src/components/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="flex items-center h-screen">
+    <div class="container mx-auto px-4">
+      <div class="flex flex-wrap -mx-4">
+        <!-- Card 1 -->
+        <div class="w-full md:w-4/12 px-4 mb-4 md:mb-10">
+          <div
+            class="bg-white p-6 border border-gray-200 rounded-lg shadow max-h-72 overflow-y-auto"
+          >
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+              Card 1
+            </h5>
+            <p class="font-normal text-gray-700">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content. Here is some additional text to
+              demonstrate what happens when the content is too long and exceeds
+              the maximum height of the card. The card should now be scrollable.
+            </p>
+          </div>
+        </div>
+        <!-- Card 2 -->
+        <div class="w-full md:w-4/12 px-4 mb-4 md:mb-0">
+          <div
+            class="bg-white p-6 border border-gray-200 rounded-lg shadow max-h-72 overflow-y-auto"
+          >
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+              Card 2
+            </h5>
+            <p class="font-normal text-gray-700">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content. Here is some additional text to
+              demonstrate what happens when the content is too long and exceeds
+              the maximum height of the card. The card should now be scrollable.
+            </p>
+          </div>
+        </div>
+        <!-- Card 3 -->
+        <div class="w-full md:w-4/12 px-4">
+          <div
+            class="bg-white p-6 border border-gray-200 rounded-lg shadow max-h-72 overflow-y-auto"
+          >
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+              Card 3
+            </h5>
+            <p class="font-normal text-gray-700">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content. Here is some additional text to
+              demonstrate what happens when the content is too long and exceeds
+              the maximum height of the card. The card should now be scrollable.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
