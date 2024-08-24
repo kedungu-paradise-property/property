2c66410d7425bcd7932a01736b73355b51835b91
nobelsaputra <kedunguparadise@gmail.com>
2024-08-24 16:03:32 +0800

    test

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -32,7 +32,7 @@
                   Rp13,900,000/are/year
                 </span>
                 <a
-                  href="#"
+                  href="components-2/Riverside.vue"
                   class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   Detail
